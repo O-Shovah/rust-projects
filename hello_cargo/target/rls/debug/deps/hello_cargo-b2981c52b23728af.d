4409b672a52d248914be86799d8134ed7a259eb9
Jens Schmelkus <github@shmelkus.net>
2021-02-12 00:05:37 +0100

    Test sub

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-b2981c52b23728af.d
@@ -0,0 +1,5 @@
+c:\Software\rust-projects\hello_cargo\target\rls\debug\deps\hello_cargo-b2981c52b23728af.rmeta: src\main.rs
+
+c:\Software\rust-projects\hello_cargo\target\rls\debug\deps\hello_cargo-b2981c52b23728af.d: src\main.rs
+
+src\main.rs:
